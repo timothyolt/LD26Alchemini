f435a06c736cd399009b69a6a55cfed885c80b30
Timothy Oltjenburns <timFothyolt@gmail.com>
2013-04-28 23:36:53 -0400

    Updated tileset

--- src/main/java/me/darkeh/projectd/assets/Tex.java
+++ src/main/java/me/darkeh/projectd/assets/Tex.java
@@ -1,5 +1,12 @@
 package me.darkeh.projectd.assets;
 
 public class Tex {
-
+    private int height;
+    private int width;
+    private int tileHeight;
+    private int tileWidth;
+    private ByteArray image;
+    public Tex(int height, int width, int tileWidth, int tileHeight){
+        this.heigh = height
+    }
 }
